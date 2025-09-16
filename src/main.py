38771f308c9363e80b6cd9111b9b7f1996964c93
Junran <junrany27@gmail.com>
2025-09-16 16:39:12 -0400

    create main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#---- in main.py (better to be another file in pycharm) ----------
+# Step 0: find P_1 and P_2 as (0,0) and (min_distance, 0). find g1 and g2 accordingly.
+# Step 0.5 (optional maybe): use [all_points] to find all points of one/each vehicle at the current time
+# Step 1: simulation:
+#   for t in range(0, 10000000):
+#       [movement]
+#       [all_points]
+#       [min_dis]
+#       [relative_speed]
+#       if relative_speed == 0:
+#           ... and break
+#   calculate d1, d2, dm_n etc.
